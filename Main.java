673a4d0fc3ab67054f34a6ed7cfc8bdf931fc4b4
Code-Sing <codysingairforce@gmail.com>
2024-10-15 10:04:57 +0000

    Completed Lab 06

--- Main.java
+++ Main.java
@@ -1,32 +1,31 @@
 import java.util.Scanner;
 
-public class Main
-{
+public class Main {
 	public static final String[] STATEMENTS = {
-		"English is my native language.",
-		"My parents graduated college.",
-		"I have never wondered where my next meal will come from.",
-		"I have no disabilities.",
-		"My work and school holidays coincide with the religious holidays I celebrate.",
-		"I studied the culture and history of my ancestors in elementary school.",
-		"I have never been bullied or been made fun of based on something I could not change (ie. race, ethnicity, sexual orientation, disabilities.)",
-		"I have never been stopped by law enforcement due to mere suspicion as opposed to legitimate wrongdoing.",
-		"I or my parents have inherited money or property.",
-		"I am a US citizen.",
-		"I feel safe going for a walk alone.",
-		"I go by the same name I was given at birth.",
-		"I am comfortable presenting my ID because it properly identifies me.",
-		"My ancestors were not forced to come to the United States against their will to be enslaved.",
-		"I have family or friends that can give me employment if I need it.",
-		"I have never been told my natural hair looks dirty or unprofessional.",
-		"I have gone to private school.",
-		"I can easily find souvenirs with my name on them."
+			"English is my native language.",
+			"My parents graduated college.",
+			"I have never wondered where my next meal will come from.",
+			"I have no disabilities.",
+			"My work and school holidays coincide with the religious holidays I celebrate.",
+			"I studied the culture and history of my ancestors in elementary school.",
+			"I have never been bullied or been made fun of based on something I could not change (ie. race, ethnicity, sexual orientation, disabilities.)",
+			"I have never been stopped by law enforcement due to mere suspicion as opposed to legitimate wrongdoing.",
+			"I or my parents have inherited money or property.",
+			"I am a US citizen.",
+			"I feel safe going for a walk alone.",
+			"I go by the same name I was given at birth.",
+			"I am comfortable presenting my ID because it properly identifies me.",
+			"My ancestors were not forced to come to the United States against their will to be enslaved.",
+			"I have family or friends that can give me employment if I need it.",
+			"I have never been told my natural hair looks dirty or unprofessional.",
+			"I have gone to private school.",
+			"I can easily find souvenirs with my name on them."
 	};
 	public static final int PTS_PER_ANSWER = 10, TOTAL_PTS_POSSIBLE = PTS_PER_ANSWER * STATEMENTS.length,
-		MAX = Person.DEFAULT_PRIVILEGE + TOTAL_PTS_POSSIBLE,
-		MIN = Person.DEFAULT_PRIVILEGE - TOTAL_PTS_POSSIBLE,
-		LEFT_WIDTH = Math.abs(MIN)/PTS_PER_ANSWER,
-		RIGHT_WIDTH = MAX/PTS_PER_ANSWER,
+			MAX = Person.DEFAULT_PRIVILEGE + TOTAL_PTS_POSSIBLE,
+					MIN = Person.DEFAULT_PRIVILEGE - TOTAL_PTS_POSSIBLE,
+			LEFT_WIDTH = Math.abs(MIN) / PTS_PER_ANSWER,
+					RIGHT_WIDTH = MAX / PTS_PER_ANSWER,
 			NAME_WIDTH = 20;
 
 	public static final String PRIVILEGE_THAN = " privilege than ";
@@ -36,41 +35,40 @@ public class Main
 
 	private static Scanner keyboard = new Scanner(System.in);
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		// DECLARATION + INITIALIZATION
 		Person p1 = new Person("Amira", "She/Her/Hers", "I am a Syrian refugee.", 40);
 		Person p2 = new Person("D'Andra", "She/Her/Hers", "I am an African-American trans woman.", -20);
 		Person p3 = new Person("Jennifer", "They/Them/Their(s)", "I am a New Yorker", 140);
 		Person p4 = new Person("Pete", "He/Him/His", "I am a guy from Pennsylvania", 200);
 		Person self = new Person();
-		Person[] people = {p1, p2, p3, p4, self};
+		Person[] people = { p1, p2, p3, p4, self };
 		boolean done = false;
 		int input;
-		
+
 		// WELCOME + INTRO
 		System.out.println("Welcome to the Privilege Calculator.\n\n"
 				+ "This is a small exercise that gives us a glimpse at how "
 				+ "fortunate we have been in life.\n");
 
 		Main.fillInfo(self);
-		
+
 		// INPUT + CALCULATION + OUTPUT
-		do{
+		do {
 			System.out.println("\n~~~Main Menu~~~\n");
 			System.out.println("1. Take questionnaire to calculate privilege estimate.");
-			System.out.println("2. Check my estimate. (Defaults to " + Person.DEFAULT_PRIVILEGE + " if questionnaire has not been taken.)");
+			System.out.println("2. Check my estimate. (Defaults to " + Person.DEFAULT_PRIVILEGE
+					+ " if questionnaire has not been taken.)");
 			System.out.println("3. Compare my estimate with others'.");
 			System.out.println("4. Exit program.");
 			System.out.print("What would you like to do?\nEnter choice: ");
-			
+
 			input = keyboard.nextInt();
 			System.out.println();
 
-			switch(input)
-			{
+			switch (input) {
 				case 1:
-					self.setPrivilege( Main.doPrivilegeQuestionnaire() );
+					self.setPrivilege(Main.doPrivilegeQuestionnaire());
 					System.out.println("Your privilege estimate is: " + self.getPrivilege());
 					System.out.println("\nReturning to main menu...\n");
 					break;
@@ -78,55 +76,65 @@ public class Main
 					System.out.println(self);
 					break;
 				case 3:
+					// Hacker Chanllenge
+					char[] firstInitial = Main.CreateNameFirstLetterArray(people);
+					System.out.println(self.getName() + "(" + firstInitial[4] + ")" + " has " + self.getPrivilege()
+							+ " privilege. How does that compare?");
+					System.out.println(Main.barChart(Main.CreatePrivilegeArray(people), firstInitial));
 					for (int i = 0; i < people.length - 1; i++) {
 						if (self.compareTo(people[i]) < 0) {
-							System.out.println("Less" + PRIVILEGE_THAN + people[i].getName());
+							System.out.println(
+									"Less" + PRIVILEGE_THAN + people[i].getName() + "(" + firstInitial[i] + ")"
+											+ " with " + people[i].getPrivilege());
 						} else if (self.compareTo(people[i]) > 0) {
-							System.out.println("More" + PRIVILEGE_THAN + people[i].getName());
+							System.out.println(
+									"More" + PRIVILEGE_THAN + people[i].getName() + "(" + firstInitial[i] + ")"
+											+ " with " + people[i].getPrivilege());
 						} else {
-							System.out.println("Equal privilege to " + people[1].getName());
+							System.out
+									.println("Equal privilege to " + people[1].getName() + "(" + firstInitial[i] + ")"
+											+ " with " + people[i].getPrivilege());
 						}
 					}
-					
-					System.out.println("\nReturning to main menu.\n");
+
+					System.out.println("\nScroll up to see comparsion.\n");
 					break;
 				case 4:
 					System.out.println("Exiting Program...\n");
-					keyboard.close(); //housekeeping
+					keyboard.close(); // housekeeping
 					done = true;
 					break;
 				default:
 					System.out.println("Invalid input, please enter a valid choice."
 							+ "\nReturning to main menu...\n");
-					break;				
+					break;
 			}
-		}while(!done);
+		} while (!done);
 
 		System.out.println("Thank you for exploring your privilege, it can be uncomfortable but it's a crucial step " +
-			"in our own growth and self-reflection. We appreciate you taking that journey with us! :D");
+				"in our own growth and self-reflection. We appreciate you taking that journey with us! :D");
 	}
 
-
-	public static void fillInfo(Person person){
-		//sets default privilege prior to questionnaire to 100
+	public static void fillInfo(Person person) {
+		// sets default privilege prior to questionnaire to 100
 		String name, pronouns, background;
-		
+
 		System.out.println("What is your name? ");
 		name = keyboard.nextLine();
 		System.out.println("\nHello " + name + "!"
 				+ "\nHere is a list of common examples of preferred pronouns:"
-				+ "\nGender Neutral/Nonbinary: they/them/thier(s) or ze/hir/hirs"
+				+ "\n Gender Neutral/Nonbinary: They/Them/Thier(s) or ze/hir/hirs"
 				+ "\n Feminie: she/her/hers"
 				+ "\n Masculine: he/him/hers"
 				+ "What are your prefered pronouns?");
 		pronouns = keyboard.nextLine();
 		System.out.println(
 				"Now please write a small self-identifying statement about yourself "
-				+ "and your background and identity, this can be anything you like!\n"
-				+ "For example: I'm a [nationality / place of origin / ethnicity / sexuality / gender expression / etc.]...");
+						+ "and your background and identity, this can be anything you like!\n"
+						+ "For example: I'm a [nationality / place of origin / ethnicity / sexuality / gender expression / etc.]...");
 		System.out.println("Tell us about yourself: ");
 		background = keyboard.nextLine();
-		
+
 		person.setName(name);
 		person.setPronouns(pronouns);
 		person.setBackground(background);
@@ -166,12 +174,27 @@ public class Main
 		return privilegeEstimate;
 	}
 
+	// Hacker Challenge Methods
+
+	/**
+	 * Creates a bar chart of privilege to visualize privilege comparsion
+	 * by concatenating a string (not quite how it works alot of trail and error)
+	 * 
+	 * @param privilege       int[], integer value of person privilege from
+	 *                        CreatePrivilegeArray method
+	 * @param nameFirstLetter char[], char of frist letter in person name from
+	 *                        CreateNameFirstLetterArray method
+	 * @return String, the barchart of privilege
+	 */
 	public static String barChart(int[] privilege, char[] nameFirstLetter) {
 		String blockLine = "";
 		String spacer = "";
 		String chart = "\n";
 		int linesOverZero = 7;
+		// "Draws" each horizontal line of the chart one half at a time
 		for (int line = 0; line < 1; line++) {
+			// Makes half of bar chart above zero by breaking privilege into 40 point block.
+			// If less then 40 half block is uesd
 			for (int j = 0; j < linesOverZero; j++) {
 				for (int i = 0; i < privilege.length; i++) {
 					int addBlock = privilege[i] / 40;
@@ -184,11 +207,12 @@ public class Main
 					} else {
 						blockLine += "    ";
 					}
-					}
+				}
 				chart += blockLine + "\n";
 				blockLine = "";
 			}
-
+			// Makes half of bar chart below zero by breaking privilege into 40 point block.
+			// If less then 40 half block is uesd
 			for (int spacerIndex = 0; spacerIndex < nameFirstLetter.length; spacerIndex++) {
 				spacer += "(" + nameFirstLetter[spacerIndex] + ")-";
 			}
@@ -212,5 +236,33 @@ public class Main
 			}
 		}
 		return chart;
+	}
+
+	/**
+	 * Creates a int[] of privilege scores from the people array
+	 * 
+	 * @param people Person[], All person objects to be compared to
+	 * @return int[], integer value of person privilege
+	 */
+	public static int[] CreatePrivilegeArray(Person[] people) {
+		int[] privilege = new int[people.length];
+		for (int i = 0; i < people.length; i++) {
+			privilege[i] = people[i].getPrivilege();
+		}
+		return privilege;
+	}
+
+	/**
+	 * Creates a char[] of the 1st letter of a persons name w/ the people array
+	 * 
+	 * @param people Person[], All person objects to be compared to
+	 * @return char[], char of frist letter in person name
+	 */
+	public static char[] CreateNameFirstLetterArray(Person[] people) {
+		char[] nameFirstLetter = new char[people.length];
+		for (int i = 0; i < people.length; i++) {
+			nameFirstLetter[i] = people[i].getName().charAt(0);
+		}
+		return nameFirstLetter;
 	}
 }
--- Person.java
+++ Person.java
@@ -1,13 +1,36 @@
+/**
+ * Person Class which implements comparable
+ * 
+ * @author Cody Singleton
+ * @version 1.0 10/13/24
+ */
 public class Person implements Comparable {
+
+	/**
+	 * Idenity Class: inner class of Person
+	 * 
+	 * @author Cody Singleton
+	 * @version 1.0 10/13/24
+	 */
 	public class Identity {
 		// IDENTITY INSTANCE VARIABLES
 		private String pronouns, background;
 
 		// IDENTITY CONSTRUCTORS
+
+		/**
+		 * Default Constructor
+		 */
 		public Identity() {
 			this(DEFAULT_PRONOUNS, DEFAULT_BACKGROUND);
 		}
 
+		/**
+		 * Full Constructor
+		 * 
+		 * @param pronouns   String, pronouns of user
+		 * @param backgorund String, background of user
+		 */
 		public Identity(String pronouns, String backgorund) {
 			if (!this.setAll(pronouns, backgorund)) {
 				System.out.println("Identity full constructor error");
@@ -15,6 +38,13 @@ public class Person implements Comparable {
 		}
 
 		// IDENTITY SETTERS
+
+		/**
+		 * Sets pronouns
+		 * 
+		 * @param pronouns String, pronouns of user
+		 * @return boolean, true if string is not null and greater than 0
+		 */
 		public boolean setPronouns(String pronouns) {
 			boolean isValid;
 			if (pronouns != null) {
@@ -27,6 +57,12 @@ public class Person implements Comparable {
 			return isValid;
 		}
 
+		/**
+		 * Sets backgrounds
+		 * 
+		 * @param backgorund String, backgrounds of user
+		 * @return boolean, true if string is not null and greater than 0
+		 */
 		public boolean setBackground(String backgorund) {
 			boolean isValid;
 			if (backgorund != null) {
@@ -39,26 +75,56 @@ public class Person implements Comparable {
 			return isValid;
 		}
 
+		/**
+		 * Sets all instance varibales
+		 * 
+		 * @param pronouns   String, pronouns of user
+		 * @param background String, background of user
+		 * @return boolean, true if both pronouns and background strings are in not null
+		 *         and greater than 0
+		 */
 		public boolean setAll(String pronouns, String background) {
 			return this.setPronouns(pronouns) && this.setBackground(background);
 		}
 
 		// IDENTITY GETTERS
+
+		/**
+		 * Gets pronouns
+		 * 
+		 * @return String, pronouns instance variable
+		 */
 		public String getPronouns() {
 			return this.pronouns;
 		}
 
+		/**
+		 * Gets background
+		 * 
+		 * @return String, background instance variable
+		 */
 		public String getBackground() {
 			return this.background;
 		}
 
 		// IDENTITY OTHER METHODS
+
+		/**
+		 * Creates a string of all instance variables
+		 * 
+		 * @return String, all instance varibales
+		 */
 		@Override
 		public String toString() {
 			return "Pronouns for " + Person.this.getName() + " are " + this.pronouns + "\n and " + Person.this.getName()
 					+ "'s background is: " + this.background;
 		}
 
+		/**
+		 * Compares object to Idenity object to see if all instance variables are equal
+		 * 
+		 * @return boolean, true if all instance variables are equal
+		 */
 		@Override
 		public boolean equals(Object other) {
 			if (other == null || (!(other instanceof Identity))) {
@@ -82,14 +148,31 @@ public class Person implements Comparable {
 	private int privilege;
 
 	// CONSTRUCTORS
+
+	/**
+	 * Full Constructor
+	 * 
+	 * @param name       String, name of person
+	 * @param pronouns   String, pronouns of user
+	 * @param background String, background of user
+	 * @param privilege  int, privilege score
+	 */
 	public Person(String name, String pronouns, String background, int privilege) {
 		this.setAll(name, pronouns, background, privilege);
 	}
 
+	/**
+	 * Default Constructor
+	 */
 	public Person() {
 		this(DEFAULT_NAME, DEFAULT_PRONOUNS, DEFAULT_BACKGROUND, DEFAULT_PRIVILEGE);
 	}
 
+	/**
+	 * Copy Constructor
+	 * 
+	 * @param original Person, object to be replicated
+	 */
 	public Person(Person original) {
 		if (original == null) {
 			throw new IllegalArgumentException("Cannot copy null obect in Person copy constructor");
@@ -99,22 +182,51 @@ public class Person implements Comparable {
 	}
 
 	// MUTATORS/SETTERS
+
+	/**
+	 * Sets name of person
+	 * 
+	 * @param name String, name of person
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Sets pronouns
+	 * 
+	 * @param pronouns String, pronouns of user
+	 */
 	public void setPronouns(String pronouns) {
 		story.setPronouns(pronouns);
 	}
 
+	/**
+	 * Sets background
+	 * 
+	 * @param background String, background of user
+	 */
 	public void setBackground(String background) {
 		story.setPronouns(background);
 	}
 
+	/**
+	 * Sets privilege score
+	 * 
+	 * @param privilege int, privilege score
+	 */
 	public void setPrivilege(int privilege) {
 		this.privilege = privilege;
 	}
 
+	/**
+	 * Sets all instance variable
+	 * 
+	 * @param name       String, name of person
+	 * @param pronouns   String, pronouns of user
+	 * @param background String, background of user
+	 * @param privilege  int, privilege score
+	 */
 	public void setAll(String name, String pronouns, String background, int privilege) {
 		this.setPrivilege(privilege);
 		this.setName(name);
@@ -123,29 +235,61 @@ public class Person implements Comparable {
 	}
 
 	// ACCESSORS / GETTERS
+
+	/**
+	 * Gets name
+	 * 
+	 * @return String, name of person
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Gets pronouns
+	 * 
+	 * @return String, pronouns of user
+	 */
 	public String getPronouns() {
 		return this.story.pronouns;
 	}
 
+	/**
+	 * Gets backgrounds
+	 * 
+	 * @return String, background of user
+	 */
 	public String getBackground() {
 		return this.story.background;
 	}
 
+	/**
+	 * Gets privilege
+	 * 
+	 * @return int, privilege score
+	 */
 	public int getPrivilege() {
 		return this.privilege;
 	}
 
 	// OTHER REQUIRED METHODS
+
+	/**
+	 * Creates a string of all instance variables
+	 * 
+	 * @return String, all instance varibales
+	 */
 	@Override
 	public String toString() {
 		return "My name is " + this.name + " and " + this.story + "\n"
 				+ "According to this calculator I ended up with " + this.privilege + " estimated privilege points";
 	}
 
+	/**
+	 * Compares object to Person object to see if all instance variables are equal
+	 * 
+	 * @return boolean, true if all instance variables are equal
+	 */
 	@Override
 	public boolean equals(Object other) {
 		if (other == null || (!(other instanceof Person))) {
@@ -159,6 +303,15 @@ public class Person implements Comparable {
 	}
 
 	// INTERFACE METHODS
+
+	/**
+	 * Interface to compare the privilege estimates of a Person object to another
+	 * Person object.
+	 * 
+	 * @return int, integer > 0 if privilege is lower, 0 if privilege is equal, and
+	 *         interger < 0 if privilege is
+	 *         greater then passed Person object
+	 */
 	@Override
 	public int compareTo(Object o) {
 		if (o == null) {
